b13efd68fb5f7bdbb18dc897e661c4444b614c03
Dhanush M <mdhanushraju345@gmail.com>
2025-09-02 22:09:03 +0530

    removed

--- src/asscets/original/Api.jsx
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-
-export default async function Api() {
-  const response = await axios.get("https://www.themealdb.com/api/json/v1/1/list.php?a=list")
-  return response.data.categories  
-}
